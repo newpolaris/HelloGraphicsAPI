1fc96c163ff9b8f912fb20ac386a454a62ab6cea
newpolaris <newpolaris@gmail.com>
2019-05-23 02:18:19 +0900

    misc

--- source/lib3d/OpenGL/gl_context.cpp
+++ source/lib3d/OpenGL/gl_context.cpp
@@ -151,10 +151,6 @@ void GLContext::setCullFaceEnable(bool enable)
 
 void GLContext::beginRendering()
 {
-    // TODO:
-    // glClearColor(0.5f, 0.5f, 0.5f, 1.f);
-    // TODO:
-    // glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
 void GLContext::endRendering()
