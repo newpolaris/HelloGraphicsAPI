701b4a0a209c1eaedca323fef4862be41b91022e
newpolaris <newpolaris@gmail.com>
2019-06-03 03:45:07 +0900

    WIP

--- sample/bunny2/bunny.cpp
+++ sample/bunny2/bunny.cpp
@@ -1,8 +1,6 @@
-#include <el/predefines.h>
-#include <el/debug.h>
-#include <OpenGL/gl_headers.h>
-
-#include <GLFW/glfw3.h>
+#include <Windows.h>
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,8 +10,13 @@
 #include <array>
 #include <chrono>
 
-#include <getopt.h>
-#include <linmath.h>
+#include <cstdio>
+#include <cmath>
+#include <random>
+#include <memory>
+
+#include <glad/glad.h>
+#include <el/debug.h>
 
 #include <el/graphics_device.h>
 #include <el/graphics_context.h>
@@ -24,21 +27,16 @@
 #include <el/graphics_texture.h>
 #include <el/graphics_data.h>
 #include <el/graphics_input_layout.h>
-#include "graphics_window.h"
-
-#include <cstdio>
-#include <cmath>
-#include <random>
-
 #include <el/utility.h>
 #include <el/image.h>
-#include <memory>
+
 #include <linmath.h>
+#include <mesh.h>
+#include <native_window_helper.h>
 
-#include "mesh.h"
-
-// TODO:
 #include <OpenGL/gl_profile.h>
+
+#pragma comment(lib, "Opengl32.lib")
 
 namespace el {
 
@@ -85,6 +83,7 @@ void main()
         return degrees * pi / 180.f;
     }
 
+    /*
     const std::string getResourcePath()
     {
     #if EL_PLAT_IOS
@@ -92,50 +91,198 @@ void main()
     #elif EL_PLAT_ANDROID
         return "";
     #else
-        return "";// EL_DEFINE_RESOURCE_PATH;
+        return EL_DEFINE_RESOURCE_PATH;
     #endif
     }
 
     const std::string getSamplePath()
     {
-        return ""; // EL_DEFINE_SAMPLE_PATH;
+        return EL_DEFINE_SAMPLE_PATH;
     }
+    */
+} // namespace el
 
-    GLFWwindow* _get(const GraphicsWindowPtr& ptr)
-    {
-        auto glfw = std::static_pointer_cast<GraphicsWindowGLFW>(ptr);
-        if (glfw == nullptr)
-            return nullptr;
-        return glfw->_window;
-    }
+#define WGL_CONTEXT_MAJOR_VERSION_ARB 0x2091
+#define WGL_CONTEXT_MINOR_VERSION_ARB 0x2092
+#define WGL_CONTEXT_FLAGS_ARB 0x2094
+#define WGL_CONTEXT_PROFILE_MASK_ARB 0x9126
+#define WGL_CONTEXT_CORE_PROFILE_BIT_ARB 0x00000001
+#define WGL_CONTEXT_COMPATIBILITY_PROFILE_BIT_ARB 0x00000002
+#define WGL_CONTEXT_DEBUG_BIT_ARB 0x00000001
+#define WGL_CONTEXT_FORWARD_COMPATIBLE_BIT_ARB 0x00000002
 
-} // namespace el {
+typedef HGLRC (WINAPI * PFNWGLCREATECONTEXTATTRIBSARBPROC)(HDC,HGLRC,const int*);
 
-int execute()
+namespace el {
+
+struct ContextRecovery
 {
-    using namespace el;
+    ContextRecovery();
+    ~ContextRecovery();
 
-    GraphicsWindowPtr windows[2];
-    GraphicsContextPtr context[2];
+    HDC _dc;
+    HGLRC _context;
+};
 
-    GraphicsWindowDesc window_desc[2];
-    window_desc[0].setWidth(1024);
-    window_desc[0].setHeight(768);
-    window_desc[0].setWindowTitle("First");
-    window_desc[0].setDeviceType(GraphicsDeviceTypeOpenGL);
-    window_desc[0].setWindowType(GraphicsWindowTypeGLFW);
+ContextRecovery::ContextRecovery()
+{
+    _dc = wglGetCurrentDC();
+    _context = wglGetCurrentContext();
+}
 
-    windows[0] = createWindow(window_desc[0]);
-    if (!windows[0]) 
+ContextRecovery::~ContextRecovery()
+{
+    wglMakeCurrent(_dc, _context);
+}
+
+struct Swapchain
+{
+    bool create(void* nativeWindow, void* nativeSwapchain);
+    void destroy();
+
+    void makeCurrent();
+    void swapbuffer();
+
+    HDC _hdc;
+    HWND _hwnd;
+    HGLRC _context;
+};
+
+void Swapchain::destroy()
+{
+    wglMakeCurrent(NULL, NULL);
+    _context = NULL;
+    if (_hdc) {
+        ReleaseDC(_hwnd, _hdc);
+        _hdc = NULL;
+    }
+    _hwnd = NULL;
+}
+
+bool Swapchain::create(void* nativeWindow, void* nativeSwapchain)
+{
+    ContextRecovery _;
+
+    _hwnd = (HWND)nativeWindow;
+    _hdc = (HDC)nativeSwapchain;
+    HGLRC dummyContext = wglCreateContext(_hdc);
+    if (!dummyContext)
         return false;
 
-    windows[0]->makeContextCurrent();
+    if (!wglMakeCurrent(_hdc, dummyContext))
+    {
+        wglDeleteContext(dummyContext);
+        return false;
+    }
 
-    GraphicsDeviceDesc deviceDesc;
-    deviceDesc.setType(GraphicsDeviceTypeOpenGL);
+    PFNWGLCREATECONTEXTATTRIBSARBPROC wglCreateContextAttribs =
+        (PFNWGLCREATECONTEXTATTRIBSARBPROC)wglGetProcAddress("wglCreateContextAttribsARB");
 
-    GraphicsDevicePtr device = createDevice(deviceDesc);
-    context[0] = device->createContext();
+    if (wglCreateContextAttribs)
+    {
+        int attribs[] = {
+            WGL_CONTEXT_MAJOR_VERSION_ARB, 4,
+            WGL_CONTEXT_MINOR_VERSION_ARB, 1,
+            WGL_CONTEXT_FLAGS_ARB, WGL_CONTEXT_DEBUG_BIT_ARB,
+            WGL_CONTEXT_PROFILE_MASK_ARB, WGL_CONTEXT_COMPATIBILITY_PROFILE_BIT_ARB,
+            0, 0
+        };
+
+        _context = wglCreateContextAttribs(_hdc, nullptr, attribs);
+    }
+    else
+    {
+        _context = wglCreateContext(_hdc);
+    }
+
+    wglMakeCurrent(_hdc, _context);
+    gladLoadGL();
+    return true;
+}
+
+void Swapchain::makeCurrent()
+{
+    wglMakeCurrent(_hdc, _context);
+}
+
+void Swapchain::swapbuffer()
+{
+    SwapBuffers(_hdc);
+}
+
+struct App
+{
+    virtual bool setup();
+    virtual void cleanup();
+
+    void activate();
+    void swapbuffer();
+    void run();
+
+    virtual void render() = 0;
+
+    Swapchain _swapchain;
+    SDL_Window* _window;
+};
+
+bool App::setup()
+{
+    SDL_Init(SDL_INIT_VIDEO);
+
+    _window = SDL_CreateWindow("bunny-sample",
+            SDL_WINDOWPOS_UNDEFINED,
+            SDL_WINDOWPOS_UNDEFINED,
+            1280, 768,
+            SDL_WINDOW_RESIZABLE);
+
+    if (_window == nullptr)
+        return false;
+
+    void* nativeWindow = ::getNativeWindow(_window);
+    if (!_swapchain.create(nativeWindow, nativeWindow))
+        return false;
+
+    return true;
+}
+
+void App::cleanup()
+{
+    _swapchain.destroy();
+
+    SDL_DestroyWindow(_window);
+    SDL_Quit();
+}
+
+void App::activate()
+{
+    _swapchain.makeCurrent();
+}
+
+void App::swapbuffer()
+{
+    _swapchain.swapbuffer();
+}
+
+void App::run()
+{
+    while (true)
+    {
+        bool isQuit = false;
+        SDL_Event e;
+        while (SDL_PollEvent(&e))
+            isQuit = (e.type == SDL_QUIT);
+        if (isQuit) break;
+
+        activate();
+        render();
+        swapbuffer();
+    }
+}
+
+struct BunnyApp : public App
+{
+    const float fNear = 0.1f;
+    const float fFar = 1000.f;
 
     GraphicsShaderPtr vertex_shader;
     GraphicsShaderPtr fragment_shader;
@@ -145,6 +292,15 @@ int execute()
     GraphicsDataPtr index_buffer;
     GraphicsInputLayoutPtr input_layout;
 
+    bool setup() override;
+    virtual void render() override;
+};
+
+bool BunnyApp::setup()
+{
+    if (!App::setup()) 
+        return false;
+
     const std::string objfiles[] = {
 		"kitten.obj",
 		"rabbit.obj",
@@ -152,8 +308,9 @@ int execute()
     };
     Geometry geometry;
     for (uint32_t i = 0; i < el::countof(objfiles); i++)
-        EL_ASSERT(LoadMesh(&geometry, el::getResourcePath() + objfiles[i]));
+        EL_ASSERT(LoadMesh(&geometry, objfiles[i]));
 
+#if 0
     // Create the OpenGL objects inside the first context, created above
     // All objects will be shared with the second context, created below
     {
@@ -208,12 +365,6 @@ int execute()
         input_layout = device->createInputLayout(input_layout_desc);
     }
 
-    // TODO:
-    // context[0]->setInputLayout(input_layout);
-    // context[0]->setVertexBuffer(0, vertex_buffer);
-
-    const float fNear = 0.1f;
-    const float fFar = 1000.f;
     std::default_random_engine eng(10);
     std::uniform_real_distribution<float> urd(0, 1);
     const uint32_t draw_count = 2000;
@@ -233,121 +384,55 @@ int execute()
         quat_rotate(draws[i].orientation, angle, axis); 
         draws[i].meshIndex = static_cast<uint32_t>(urd(eng) * geometry.meshes.size());
     }
-
-    context[0]->setProgram(program);
-
-    GLProfileBusyWait profile[2];
-    profile[0].setName("window 0");
-    profile[0].setDevice(device);
-    profile[0].create();
-    profile[1].setName("window 1");
-    profile[1].setDevice(device);
-    profile[1].create();
-
-    window_desc[1].setWidth(400);
-    window_desc[1].setHeight(400);
-    window_desc[1].setWindowTitle("Second");
-    window_desc[1].setDeviceType(GraphicsDeviceTypeOpenGL);
-    window_desc[1].setWindowType(GraphicsWindowTypeGLFW);
-    windows[1] = createWindow(window_desc[1]);
-    if (!windows[1])
-        return false;
-
-    // Place the second window to the right of the first
-    {
-        int xpos, ypos, left, right, width;
-
-        glfwGetWindowSize(_get(windows[0]), &width, NULL);
-        glfwGetWindowFrameSize(_get(windows[0]), &left, NULL, &right, NULL);
-        glfwGetWindowPos(_get(windows[0]), &xpos, &ypos);
-        glfwSetWindowPos(_get(windows[1]), xpos + width + left + right, ypos);
-    }
-
-    windows[1]->makeContextCurrent();
-
-    // While objects are shared, the global context state is not and will
-    // need to be set up for each context
-    // context[1] = device->createDeviceContext();
-    // context[1]->setProgram(program);
-
-    while (!glfwWindowShouldClose(_get(windows[0]))
-        && !glfwWindowShouldClose(_get(windows[1])))
-    {
-        int i;
-        for (i = 0; i < 1; i++)
-        {
-            windows[i]->makeContextCurrent();
-
-            auto width = windows[i]->getWidth();
-            auto height = windows[i]->getHeight();
-
-            profile[i].start();
-
-            context[i]->beginRendering();
-
-            // TODO:
-            glClearColor(0.5f, 0.5f, 0.5f, 1.f);
-            // TODO:
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-            mat4x4 project;
-            const float aspect = static_cast<float>(width) / height;
-            mat4x4_perspective(project, radians(70.f), aspect, fNear, fFar);
-            context[i]->setUniform("uProject", project);
-            context[i]->setViewport(Viewport(0, 0, width, height));
-            context[i]->setIndexBuffer(index_buffer);
-            context[i]->setVertexBuffer("vPosition", vertex_buffer, sizeof(Vertex), offsetof(Vertex, x));
-            context[i]->setVertexBuffer("vNormal", vertex_buffer, sizeof(Vertex), offsetof(Vertex, nx));
-            context[i]->setVertexBuffer("vTexcoord", vertex_buffer, sizeof(Vertex), offsetof(Vertex, tu));
-            for (auto& draw : draws)
-            {
-                const auto& mesh = geometry.meshes[draw.meshIndex];
-                const uint32_t vertexOffsetInByte = mesh.vertexOffset * sizeof(Vertex);
-
-                context[i]->setUniform("uScale", draw.scale);
-                context[i]->setUniform("uTranslate", draw.translate);
-                context[i]->setUniform("uOrientation", draw.orientation);
-
-                // Shared bewteen context
-                context[i]->drawIndexed(GraphicsPrimitiveTypeTriangle,
-                                        mesh.indexCount,
-                                        mesh.indexOffset,
-                                        mesh.vertexOffset);
-            }
-
-            profile[i].end();
-            context[i]->endRendering();
-
-            glfwSwapBuffers(_get(windows[i]));
-
-            char profileBuf[256] = {'\0'};
-            sprintf(profileBuf, "%s CPU %.3f ms, GPU %.3f ms", 
-                    profile[i].getName().c_str(), profile[i].getCpuTime(), profile[i].getGpuTime());
-
-            glfwSetWindowTitle(_get(windows[i]), profileBuf);
-        }
-
-        glfwPollEvents();
-    }
-
-    vertex_shader.reset();
-    fragment_shader.reset();
-    program.reset();
-    vertex_buffer.reset();
-    index_buffer.reset();
-    context[0].reset();
-    context[1].reset();
-    device.reset();
-    windows[1].reset();
-    windows[0].reset();
-
-    glfwTerminate();
-
+#endif
     return true;
 }
 
+void BunnyApp::render()
+{
+#if 0
+    int w, h;
+    SDL_GetWindowSize(_window, &w, &h);
+
+    glClearColor(0.5f, 0.5f, 0.5f, 1.f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    mat4x4 project;
+    const float aspect = static_cast<float>(w) / h;
+    mat4x4_perspective(project, radians(70.f), aspect, fNear, fFar);
+    context[i]->setUniform("uProject", project);
+    context[i]->setViewport(Viewport(0, 0, width, height));
+    context[i]->setIndexBuffer(index_buffer);
+    context[i]->setVertexBuffer("vPosition", vertex_buffer, sizeof(Vertex), offsetof(Vertex, x));
+    context[i]->setVertexBuffer("vNormal", vertex_buffer, sizeof(Vertex), offsetof(Vertex, nx));
+    context[i]->setVertexBuffer("vTexcoord", vertex_buffer, sizeof(Vertex), offsetof(Vertex, tu));
+    for (auto& draw : draws)
+    {
+        const auto& mesh = geometry.meshes[draw.meshIndex];
+        const uint32_t vertexOffsetInByte = mesh.vertexOffset * sizeof(Vertex);
+
+        context[i]->setUniform("uScale", draw.scale);
+        context[i]->setUniform("uTranslate", draw.translate);
+        context[i]->setUniform("uOrientation", draw.orientation);
+
+        // Shared bewteen context
+        context[i]->drawIndexed(GraphicsPrimitiveTypeTriangle,
+                                mesh.indexCount,
+                                mesh.indexOffset,
+                                mesh.vertexOffset);
+    }
+    context[i]->endRendering();
+#endif
+}
+
+} // namespace el
+
 int main(int argc, char** argv)
 {
-    EL_ASSERT(execute());
+    el::BunnyApp app;
+
+    EL_ASSERT(app.setup());
+    app.run();
+
     return 0;
 }
