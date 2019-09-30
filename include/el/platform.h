c69c33d7195a8d0e8f5c92b9ef21a4ff37218f46
newpolaris <newpolaris@gmail.com>
2019-09-30 15:29:24 +0900

    update vec plus and minus, platform define

--- include/el/platform.h
+++ include/el/platform.h
@@ -4,7 +4,6 @@
 #define EL_ARCH_ARM 0
 #define EL_ARCH_X86_32 0
 #define EL_ARCH_X86_64 0
-#define EL_ARCH_X86 0
 
 #define EL_COMP_GCC 0
 #define EL_COMP_CLANG 0
@@ -12,11 +11,11 @@
 
 #define EL_PLAT_WINDOWS 0
 #define EL_PLAT_ANDROID 0
-#define EL_PLAT_APPLE 0
 #define EL_PLAT_IOS 0
 #define EL_PLAT_IOS_DEVICE 0
 #define EL_PLAT_IOS_SIMULATOR 0
 #define EL_PLAT_OSX 0
+#define EL_PLAT_TVOS 0
 
 // https://www.boost.org/doc/libs/1_66_0/doc/html/predef
 #if defined(__arm__) || defined(__arm64) || defined(__thumb__) || \
@@ -43,10 +42,10 @@
 #   define EL_ARCH_X86_64 1
 #endif
 
-#if EL_ARCH_X86_32 || EL_ARCH_X86_64
-#   undef EL_ARCH_X86
-#   define EL_ARCH_X86 1
-#endif
+#define EL_ARCH_X86    (0 \
+    ||  EL_ARCH_X86_32    \
+    ||  EL_ARCH_X86_64    \
+    )
 
 // http://sourceforge.net/apps/mediawiki/predef/index.php?title=Compilers
 #if defined(__clang__)
@@ -72,8 +71,8 @@
 #   undef  EL_PLAT_IOS
 #   define EL_PLAT_IOS __ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__
 #elif  defined(__ENVIRONMENT_TV_OS_VERSION_MIN_REQUIRED__)
-#   undef  EL_PLAT_IOS
-#   define EL_PLAT_IOS __ENVIRONMENT_TV_OS_VERSION_MIN_REQUIRED__
+#   undef  EL_PLAT_TVOS
+#   define EL_PLAT_TVOS __ENVIRONMENT_TV_OS_VERSION_MIN_REQUIRED__
 #elif defined(__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__)
 #   undef  EL_PLAT_OSX
 #   define EL_PLAT_OSX __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__
@@ -83,16 +82,17 @@
 #    include <TargetConditionals.h>
 #    if TARGET_OS_SIMULATOR == 1
 #        undef EL_PLAT_IOS_SIMULATOR
-#        define EL_PLAT_IOS_SIMULATOR BOOST_VERSION_NUMBER_AVAILABLE
+#        define EL_PLAT_IOS_SIMULATOR __ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__
 #    else
 #        undef EL_PLAT_IOS_DEVICE
-#        define EL_PLAT_IOS_DEVICE BOOST_VERSION_NUMBER_AVAILABLE
+#        define EL_PLAT_IOS_DEVICE __ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__
 #    endif
 #endif
 
-#if EL_PLAT_ISO || EL_PLAT_OSX
-#   undef EL_PLAT_APPLE
-#   define EL_PLAT_APPLE 1
-#endif
+#define EL_PLAT_APPLE (0 \
+    ||  EL_PLAT_IOS      \
+    ||  EL_PLAT_OSX      \
+    ||  EL_PLAT_TVOS     \
+    )
 
 #endif // __EL_PLATFORM_H__
